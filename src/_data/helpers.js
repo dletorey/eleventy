473c15742ff05c8364380ffb28560beb85bcc48e
Dave Letorey <daveletorey@gmail.com>
2020-07-02 15:56:55 +0100

    2nd if statement that checks that the current item is not the home page greater than 1 and the current item is the first page of the page url, if so sets the response value. Then returns the response value

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -12,5 +12,9 @@ module.exports = {
        if (itemUrl === pageUrl) {
            response = ' aria-current="page"';
        }
+       if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+        response += ' data-state="active"';
+      }
+      return response;
    }
 }
