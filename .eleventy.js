74cc4cb3af782aa2ff55979a2b24059f2a0af697
Dave Letorey <daveletorey@gmail.com>
2020-07-03 16:27:43 +0100

    created the collections again but this time using the utility to sort

--- .eleventy.js
+++ .eleventy.js
@@ -11,5 +11,14 @@ module.exports = config => {
         output: 'dist'
       }
     };
-    
+    // Returns work items, sorted by display order
+    config.addCollection('work', collection => {
+      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', collection => {
+      return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+        x => x.data.featured
+      );
+    });
   };
