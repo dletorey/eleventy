81d62f9eda7bab5c2c7cfd3f00ca81b7be0b3c5e
Dave Letorey <daveletorey@gmail.com>
2020-07-04 15:52:03 +0100

    changed the order to make the collections work

--- .eleventy.js
+++ .eleventy.js
@@ -2,15 +2,6 @@ const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 module.exports = config => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
-    return {
-        markdownTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-      dir: {
-        input: 'src',
-        output: 'dist'
-      }
-    };
     // Returns work items, sorted by display order
     config.addCollection('work', collection => {
       return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
@@ -21,4 +12,13 @@ module.exports = config => {
         x => x.data.featured
       );
     });
+    return {
+        markdownTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+      dir: {
+        input: 'src',
+        output: 'dist'
+      }
+    };
   };
